c66e6c4d3e1eda5df1b0b6d639315f5b4ac38cea
abc123wuhan <144417076+abc123wuhan@users.noreply.github.com>
2023-09-21 23:00:43 +0800

    model.py

--- model.py
+++ model.py
@@ -14,93 +14,8 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.layers import Bidirectional
-# import time
-# start=time.clock()
-
-trainenable = 0
-day_all = 1
-data = pd.read_csv("H:\code\swarm_extended_omni_data.csv", header=None)
-data = data.get_chunk()
-# 查看数据详情
-#data
-
-glon = data[4]
-glat = data[5]
-height = data[3]
-doy = data[15]
-Kp = data[52]
-DST = data[54]
-ap = data[63]
-F107 = data[64]
-
-dens = data[8]
-dens = dens.astype("float")
-
-df_feature = data.drop([ 0, 1, 2, 8, 14, 16], axis=1)
-df_feature = df_feature.astype("float")
-data_inf = np.isfinite(df_feature).all(axis=1).squeeze()
-
-df_feature = df_feature.loc[data_inf,:]                        
-
-dens = dens[data_inf]
-dens_inf = np.isfinite(dens).squeeze()
-df_feature = df_feature.loc[dens_inf,:]
-dens = dens[dens_inf]
-# 最后再看一下数据情况
-df_feature = df_feature.reset_index(drop=True)
-#display(df_feature)
-# df_feature.shape
-
-idx = np.array(df_feature.index)
-# 划分训练集、验证集、测试集
-n_data = len(df_feature)
-train_ind = np.random.permutation(idx[:int(n_data * .8)])                   
-valid_ind = np.random.permutation(idx[int(n_data * .8):int(n_data * .9)])   
-test_ind = idx[int(n_data * .9):]                                          
-# len(test_ind)
-                                         
-train_ind_ = idx[:int(n_data * .8)]                   
-valid_ind_ = idx[int(n_data * .8):int(n_data * .9)]    
-test_ind_ = idx[int(n_data * .9):]
-a=data[0]
-print('训练数据：',a[train_ind_[0]],'-',a[train_ind_[-1]])
-print('验证数据：',a[valid_ind_[0]],'-',a[valid_ind_[-1]])
-print('测试数据：',a[test_ind_[0]],'-',a[test_ind_[-1]])
-
-if day_all:
-    start_time = '2015-02-02'
-    end_time = "2015-02-30"
-
-    # start_time = "2014-02-01" # "2014-2-11"  "2010-04-19"
-    # end_time = "2014-02-03"   # "2014-3-11"  "2010-04-29"
-
-    test_ind = data[(data[0]>=start_time) & (data[0]<=end_time)].index
-    # test_ind = data[data[0]==start_time].index
-
-else:
-    print('error')
-# print(test_ind[0],test_ind[-1],test_ind.shape)
-
-scaler_data = StandardScaler() 
-scaler_dens = StandardScaler()
-
-train_feature = df_feature.loc[train_ind]
-# train_ind：训练集的序列号
-
-scaler_data.fit(train_feature)
-feature = scaler_data.transform(df_feature)
-
-scaler_dens.fit(dens.values.reshape(-1, 1))
-dens = scaler_dens.transform(dens.values.reshape(-1, 1))
-
-# 将数据表类型转化为numpy型
-feature = np.array(feature)
-
-# 将密度数据维度进行转换，(len(data),) -> (len(data), 1)
-dens = np.reshape(dens, (-1, 1))
 
 def models(input_dim = 225):
-
 # 指定显卡
 #     import os
 #     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
@@ -108,8 +23,6 @@ def models(input_dim = 225):
     # 模型
     model = K.models.Sequential()
     feature_size = omni2_num * (history_length - 1) + swarm_nums
-
-
     model.add(K.layers.Dense(units=64, input_dim=input_dim, kernel_initializer = 'normal', activation='sigmoid'))
     # 对于TensorFlow不能表达的复杂激活函数，如含有可学习参数的激活函数，可通过高级激活函数实现，可以在 keras.layers.advanced_activations 模块中找到。
     model.add(K.layers.Dropout(0.2))
@@ -227,108 +140,6 @@ if trainenable:
               verbose=1,
               validation_split = 0.2,
               callbacks=callbacks_list)
-    
-
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import MultipleLocator
-
-plt.rcParams['figure.figsize']=(6, 4) 
-
-loss1=history1.history["loss"]
-test_loss1 = history1.history["val_loss"]
-# mse1=history1.history['mse']
-# test_mse1 = history1.history["val_mse"]
-
-plt.plot(loss1, label='train', color='r')
-
-x_major_locator=MultipleLocator(2) 
-ax=plt.gca()
-ax.xaxis.set_major_locator(x_major_locator)
-plt.xlim(-0.5,21)
-
-# 修改坐标轴字体及大小
-plt.yticks(fontproperties='Times New Roman', size=10)
-plt.xticks(fontproperties='Times New Roman', size=10)
-
-# 设置坐标轴标签及字体大小,labelpad坐标轴标签和坐标轴的距离
-plt.xlabel('Epoch', fontsize=15, labelpad=1)
-plt.ylabel('Loss', fontsize=15, labelpad=1)
-#plt.title(' ', fontsize=14)
-
-# 设置坐标刻度值的大小
-plt.tick_params(direction='in', width=1, length=2, pad=3)
-
-plt.legend(fontsize=10)
-plt.savefig("H:/论文相关/大气密度预测/论文图/rmse_epoch/loss_epoch_test.jpg",bbox_inches='tight',dpi=300,pad_inches=0.0)
-plt.show()
-
-# 转换时间
-data_time= pd.read_csv("H:/code/swarm_extended_omni_data.csv", header=None)
-ymd=data_time[0]
-hms=data_time[1]
-time1=ymd[-len(test_ind):]
-time2=hms[-len(test_ind):]
-# g=[]
-# for i in time1:
-#     i=int(i[0:4])
-#     g.append(i)
-#
-# f=[]
-# for i in time1:
-#     i=int(i[5:7])
-#     f.append(i)
-#
-j=[]
-for i in time1:
-    i=int(i[8:10])
-    j.append(i)
-
-k=[]
-for i in time2:
-    i=float(i[0:2])/24+float(i[3:5])/(24*60)+float(i[6:8])/(24*60*60)
-    k.append(i)
-
-# l=[]
-# for i in time2:
-#     i=float(i[0:2])/(24*30)+float(i[3:5])/(24*60*30)+float(i[6:8])/(24*60*60*30)
-#     l.append(i)
-
-time=[]
-for m,n in zip(j,k):
-    l=m+n
-    # l=str(l)
-    # l=l[0:3]
-    # l=float(l)
-    time.append(l)
-
-densitypre = scaler_dens.inverse_transform(BC.reshape(-1,1))
-
-densityopt = scaler_dens.inverse_transform(dens[test_ind].reshape(-1,1))
-density_pre=densitypre*1000000000000
-density_opt=densityopt*1000000000000
-
-#print(density_pre[0:1],'\n',density_opt[:1],'\n',BC.reshape(-1,1)[:1],'\n',dens[test_ind][:1])
-
-plt.rcParams['figure.figsize']=(18.8, 7.2) 
-plt.plot(time[:1000],density_opt[:1000], label='Optivision', linewidth = 2,color='b',alpha = 1)
-plt.plot(time[:1000],density_pre[:1000], label='Predition',color='r',linewidth = 2,alpha = 1)
-
-# 修改坐标轴字体及大小
-plt.yticks(fontproperties='Times New Roman', size=20)
-plt.xticks(fontproperties='Times New Roman', size=20)
-
-# 设置坐标轴标签及字体大小,labelpad坐标轴标签和坐标轴的距离
-plt.xlabel('November,2019', fontsize=20, labelpad=1)
-plt.ylabel('atmosphere density (10^-13 kg/ m^3)', fontsize=20, labelpad=1)
-#plt.title(' ', fontsize=14)
-
-# 设置坐标刻度值的大小
-plt.tick_params(direction='in', width=1, length=2, pad=3)#修改刻度线线粗细width参数
-
-# 控制图例的形状大小
-plt.legend(fontsize=10)
-plt.savefig("H:/论文相关/大气密度预测/论文图/density.jpg",bbox_inches='tight',dpi=300,pad_inches=0.0)
-plt.show()
 
 
 
